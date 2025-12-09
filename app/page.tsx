c87ae2245367dde310d13f93f16a5b4567e97346
Denis <deniskatarasenko6@gmail.com>
2025-12-09 19:11:32 +0100

    feat: update HomeScreen component with content and structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
 export default function HomeScreen() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Denis Tarasenko</h1>
+
+      <p>
+        I build <a href="https://nextnative.dev">software</a> and read a lot of{" "}
+        <a href="https://www.goodreads.com/review/list/175721134-denis-tarasenko?shelf=currently-reading">
+          books.
+        </a>
+      </p>
+
+      <p>
+        Also I recently realized that you always get a better result when you
+        remove things.
+      </p>
+
+      <p>Not add them.</p>
+    </div>
+  );
 }

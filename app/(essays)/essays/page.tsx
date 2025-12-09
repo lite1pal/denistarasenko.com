7e92f4d132cf414733be02759af3a0400ddecfa2
Denis <deniskatarasenko6@gmail.com>
2025-12-09 20:09:38 +0100

    feat: create new essay page with title and author information

---
+++ app/(essays)/essays/page.tsx
@@ -0,0 +1,17 @@
+export default function Page() {
+  return (
+    <article>
+      <h1 className="essay-title">Essays</h1>
+
+      <a className="author" href="/">
+        By Denis Tarasenko
+      </a>
+
+      <ul>
+        <li>
+          <a href="/start-before-you-are-ready">Start before you're ready</a>
+        </li>
+      </ul>
+    </article>
+  );
+}

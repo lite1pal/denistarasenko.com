8d2b008eaa2ed03ad9d1611b512d2232fd4359bc
Denis <deniskatarasenko6@gmail.com>
2025-12-09 20:09:31 +0100

    feat: add new essay page "Start before you're ready" with content and structure

---
+++ app/(essays)/start-before-you-are-ready/page.tsx
@@ -0,0 +1,77 @@
+export default function Page() {
+  return (
+    <article>
+      <h1 className="essay-title">Start before you're ready</h1>
+
+      <a className="author" href="/">
+        By Denis Tarasenko
+      </a>
+
+      <p className="mt-5">I never feel quite ready before doing something.</p>
+
+      <p>
+        {" "}
+        It might be anything from: launching a product writing a newsletter
+        (like this one) starting a Youtube channel calling my grandma And this
+        list might go on forever. Right now I’m stuck at recording a demo video
+        for my new product. I’ve done all the heavy lifting already (product,
+        landing, docs, support, payments) and this step is literally what’s
+        blocking me from moving on with marketing.
+      </p>
+      <p>
+        And what annoys me the most is that I can’t make myself do this. I keep
+        making mental excuses that I’m not good enough to make this video, my
+        English accent isn’t that perfect, and that it just makes no sense.
+        Wrong. It makes perfect sense. This is a roadblock I must get through in
+        order to progress and become mentally stronger and more resilient. Every
+        journey has one, so here’s mine.
+      </p>
+
+      <p>
+        A couple of days ago I had a walk with my friend. We usually buy some
+        sausages and marshmallows, then make a tiny fire in nature and roast
+        them. It’s delicious, lemme tell you.
+      </p>
+
+      <p>
+        So as we sat enjoying the fire, he asked me about the project I’d been
+        working on for months. I was kinda stuck and couldn’t find words to say
+        because it was embarrassing for me to acknowledge that I’d been actually
+        procrastinating on it for quite some time. So I explained to him my
+        “issue”, so to speak, with this damn demo video that kept me moveless.
+        And what he said was: “Maybe you should allocate the whole day for it,
+        like next Tuesday”
+      </p>
+
+      <p>
+        It sounded so simple and straightforward. I became motivated the second
+        he said it. I immediately took out my phone and created a new reminder
+        on Tuesday to record it. Suddenly, I remembered a quote about taking a
+        responsibility for something you struggle to do (thanks to Tim Ferriss),
+        so I suggested to my friend that we should come up with a punishment for
+        me in case I fail to get it done by Tuesday. We agreed on me sending him
+        $20 if I fail. Not much, but it'll definitely keep me accountable now.
+      </p>
+
+      <p>
+        Honestly, it felt so liberating. Today is Monday, and I’m writing a
+        script for this demo video. Tomorrow I’m spending the whole morning
+        recording it.{" "}
+        <i>
+          (update from the future: I recorded the video and next week made my
+          first ever sale)
+        </i>
+      </p>
+
+      <p>So if you’re in a similar situation like me:</p>
+
+      <ol className="list-decimal">
+        <li>call up your friend</li>
+        <li>tell about your struggle</li>
+        <li>name a deadline</li>
+        <li>suggest a payment in case you fail</li>
+        <li>finally do your thing with much less stress</li>
+      </ol>
+    </article>
+  );
+}

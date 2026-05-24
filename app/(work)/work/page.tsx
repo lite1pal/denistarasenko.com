b34e189b2d943ab9e9a4362764b9fb7de957b759
Denis Tarasenko <109691730+lite1pal@users.noreply.github.com>
2026-05-24 13:12:43 +0200

    feat: refactor work page to use dynamic data structure for projects

--- app/(work)/work/page.tsx
+++ app/(work)/work/page.tsx
@@ -1,4 +1,10 @@
-import { Byline, PageShell, PageTitle, Prose } from "@/app/components/page-shell";
+import {
+  Byline,
+  PageShell,
+  PageTitle,
+  Prose,
+} from "@/app/components/page-shell";
+import { work } from "./work.data";
 
 export default function Page() {
   return (
@@ -8,21 +14,13 @@ export default function Page() {
 
       <Prose className="mt-5">
         <ul>
-          <li>
-          <a href="https://nextnative.dev">
-            Next.js boilerplate to ship mobile apps
-          </a>
-          </li>
-          <li>
-          <a href="https://apps.apple.com/ua/app/lasting-habits/id6736766976">
-            Build habits that last
-          </a>
-          </li>
-          <li>
-          <a href="https://apps.apple.com/ua/app/sproutly-ai-plant-identifier/id6748902696">
-            Learn about plants with AI
-          </a>
-          </li>
+          {work.map((w) => (
+            <li>
+              <a href={w.href} target="_blank">
+                {w.title}{" "}
+              </a>
+            </li>
+          ))}
         </ul>
       </Prose>
     </PageShell>
---
+++ app/(work)/work/work.data.tsx
@@ -0,0 +1,19 @@
+type Work = {
+  title: string;
+  href: string;
+};
+
+export const work: Work[] = [
+  {
+    title: "Next.js boilerplate to ship mobile apps",
+    href: "https://nextnative.dev",
+  },
+  {
+    title: "Build habits that last",
+    href: "https://apps.apple.com/ua/app/lasting-habits/id6736766976",
+  },
+  {
+    title: "Learn about plants with AI",
+    href: "https://apps.apple.com/ua/app/sproutly-ai-plant-identifier/id6748902696",
+  },
+];
